310a23fc6c7388f4985acba42d651a9f0982f60c
bug1k <sergey.dev.987@gmail.com>
2025-08-09 16:31:15 +0800

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,6 @@ import "@/styles/globals.scss";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => (
   <html lang="en">
-    <head>
-      <link
-        href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;700&display=swap"
-        rel="stylesheet"
-      />
-    </head>
     <body>
       {children}
     </body>
