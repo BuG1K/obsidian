e278d102fed921dd86237e02bad47c3a0349d6a4
bug1k <sergey.dev.987@gmail.com>
2025-08-11 16:23:23 +0800

    43555435

---
+++ src/app/api/route.ts
@@ -0,0 +1,10 @@
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const phone = searchParams.get("phone");
+
+  if (phone) {
+    return new Response(phone, { status: 200 });
+  }
+
+  return new Response("no phone param", { status: 400 });
+}
