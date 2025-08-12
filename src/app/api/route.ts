9244841fd066302f1d996ccef7cc7715dc89fb32
bug1k <sergey.dev.987@gmail.com>
2025-08-12 20:55:39 +0800

    0000

---
+++ src/app/api/route.ts
@@ -0,0 +1,12 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const phone = searchParams.get("phone");
+  console.log(phone)
+  if (phone) {
+    return new Response(phone, { status: 200 });
+  }
+
+  return new Response("no phone param", { status: 400 });
+}
