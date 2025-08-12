19158a6e355fede4785269a6c91cb89d9ada9e79
bug1k <sergey.dev.987@gmail.com>
2025-08-12 20:57:21 +0800

    56456456

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,9 +1,7 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const phone = searchParams.get("phone");
-  console.log(phone)
+  console.log(phone);
   if (phone) {
     return new Response(phone, { status: 200 });
   }
